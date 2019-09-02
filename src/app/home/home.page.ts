5a435ad2949577523ed6ffb81fd8de3093b88cc3
Stolar <stolaaar@gmail.com>
2019-09-03 00:08:29 +0200

    Dynamically showing the event properties in the home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,7 +3,9 @@ import { Storage } from "@ionic/storage";
 import { NavController } from "@ionic/angular";
 import { Observable, Subscriber } from "rxjs";
 import User from "src/models/User";
+import Event from "src/models/Event";
 import { UsersService } from "../services/users.service";
+import { EventsService } from "../services/events.service";
 
 @Component({
   selector: "app-home",
@@ -12,20 +14,26 @@ import { UsersService } from "../services/users.service";
 })
 export class HomePage {
   User = {} as User;
+  Event = {} as Event;
 
   constructor(
     private storage: Storage,
     private navCtrl: NavController,
+    private eventsService: EventsService,
     private userService: UsersService
   ) {
     // Get the user from the db
   }
+
   ngOnInit() {
     this.storage
       .get("user")
       .then(userData => {
         this.userService.getUser(userData.uid).subscribe(user => {
           this.User = user;
+        });
+        this.eventsService.getEvent(userData.uid).subscribe(event => {
+          this.Event = event;
         });
       })
       .catch(err => console.log(err.message));
